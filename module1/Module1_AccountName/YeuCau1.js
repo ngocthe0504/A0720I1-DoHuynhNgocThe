9514d7ea27cab2c451a7001b1198aae6daff1b1b
ngocthe0504 <thedeptraihihi@gmail.com>
2020-07-23 18:53:47 +0700

    update

--- module1/Module1_AccountName/YeuCau1.js
+++ module1/Module1_AccountName/YeuCau1.js
@@ -1,9 +1,9 @@
 
 function bill() {
-    var typeOfHouse=document.querySelector("#typeOfHouse"); // kiểu nhà
-    var rentDays=document.querySelector("#rentDays"); // số ngày thuê kiểu text
-    var discount=document.querySelector("#discount"); // kiểu selection có value
-    var totalOfMoney;
+    let typeOfHouse=document.getElementById("typeOfHouse"); // kiểu nhà
+    let rentDays=document.getElementById("rentDays"); // số ngày thuê kiểu text
+    let discount=document.getElementById("discount"); // kiểu selection có value
+    let totalOfMoney;
     totalOfMoney = parseInt(+typeOfHouse.value) * parseInt(rentDays.value) * (1 - parseInt(discount.value) / 100);
     alert("Total of money:" + totalOfMoney);
 }
