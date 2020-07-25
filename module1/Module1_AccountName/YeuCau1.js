200f0e595a132734fcddbacef9c9be64a3c0a611
ngocthe0504 <thedeptraihihi@gmail.com>
2020-07-25 15:11:11 +0700

    update

--- module1/Module1_AccountName/YeuCau1.js
+++ module1/Module1_AccountName/YeuCau1.js
@@ -1,8 +1,7 @@
-
 function bill() {
-    let typeOfHouse=document.getElementById("typeOfHouse"); // kiểu nhà
-    let rentDays=document.getElementById("rentDays"); // số ngày thuê kiểu text
-    let discount=document.getElementById("discount"); // kiểu selection có value
+    let typeOfHouse=document.getElementById("typeOfHouse");
+    let rentDays=document.getElementById("rentDays");
+    let discount=document.getElementById("discount");
     let totalOfMoney;
     totalOfMoney = parseInt(+typeOfHouse.value) * parseInt(rentDays.value) * (1 - parseInt(discount.value) / 100);
     alert("Total of money:" + totalOfMoney);
