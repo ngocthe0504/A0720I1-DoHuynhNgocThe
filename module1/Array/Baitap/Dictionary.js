76ef5d54763f7f20aeefecadb627973b669f0414
ngocthe0504 <thedeptraihihi@gmail.com>
2020-09-04 13:24:59 +0700

    update

---
+++ module1/Array/Baitap/Dictionary.js
@@ -0,0 +1,5 @@
+let English = ["Brother","Dad","Mom","Me","Siter"];
+let Vietnamese = ["Anh trai", "Bố","Mẹ","Tôi", "Chị"];
+let getRequest = prompt("Nhập vào từ muốn tìm");
+let getPosition = English.indexOf(getRequest);
+alert(Vietnamese[getPosition]);
---
+++ module1/Loop/BaiTap/SNTDauTien.js
@@ -0,0 +1,25 @@
+let bienDem = 0;
+let soCanKtra = 0;
+let soLanHienThi = document.getElementById("count");
+let root = "";
+function hienThiSNT() {
+    while (bienDem < parseInt(soLanHienThi.value)){
+        if (kTraSNT(soCanKtra)) {
+            root += soCanKtra + "<br>";
+            bienDem++;
+        }
+        soCanKtra++
+    }
+    document.getElementById("result").innerHTML = root;
+}
+function kTraSNT(n) {
+    if (n <= 1) {
+        return false;
+    }
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
